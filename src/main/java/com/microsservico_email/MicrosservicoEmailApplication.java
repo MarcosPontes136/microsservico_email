3d21d8a7b6a4ee9ad53d1b43d4abefd49f9d1e15
MarcosPontes136 <zeuscod@outlook.com>
2025-01-09 18:44:47 -0300

    hotfix/Ajuste para build do lombok.

--- src/main/java/com/microsservico_email/MicrosservicoEmailApplication.java
+++ src/main/java/com/microsservico_email/MicrosservicoEmailApplication.java
@@ -1,13 +1,40 @@
 package com.microsservico_email;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @SpringBootApplication
 public class MicrosservicoEmailApplication {
+	
+    private static final String URL_APP = "url.aplicacao";
+    private static final String SERVER_PORT = "server.port";
+    private static final Logger LOGGER = LoggerFactory.getLogger(MicrosservicoEmailApplication.class);
 
 	public static void main(String[] args) {
-		SpringApplication.run(MicrosservicoEmailApplication.class, args);
+
+        final ConfigurableApplicationContext ctx =  SpringApplication.run(MicrosservicoEmailApplication.class, args);
+        try {
+            final Environment env = ctx.getEnvironment();
+            LOGGER.info("\n ********************** \n" + "\tAplicacao {} iniciada com sucesso!\n" + "\tDisponivel nos enderecos:\n"
+                            + "\tLocal: http://localhost:{}\n" + "\tExterno: {}\n"
+                            + "\tSwagger Url: {}\n"
+                            + "\tLocal Swagger Url: http://localhost:{}\n",
+                    env.getProperty("spring.application.name"),
+                    env.getProperty(SERVER_PORT),
+                    env.getProperty(URL_APP),
+                    env.getProperty(URL_APP) /*+ SWAGGER_UI_HTML*/,
+                    env.getProperty(SERVER_PORT) /*+ SWAGGER_UI_HTML*/);
+        } catch (final Exception e) {
+            LOGGER.error("Falha ao executar aplicacao: {}", e);
+            ctx.close();
+        }
 	}
 
 }
--- src/test/java/com/microsservico_email/MicrosservicoEmailApplicationTests.java
+++ src/test/java/com/microsservico_email/MicrosservicoEmailApplicationTests.java
@@ -1,13 +1,39 @@
 package com.microsservico_email;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.slf4j.Logger;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
 
 @SpringBootTest
 class MicrosservicoEmailApplicationTests {
+	
+    @Mock
+    private ConfigurableApplicationContext mockCtx;
 
-	@Test
-	void contextLoads() {
-	}
+    @Mock
+    private Environment mockEnv;
+
+    @Mock
+    private Logger mockLogger;
+    
+    @Test
+    public void contextLoads() {
+        assertThat(true).isTrue();
+    }
+
+
+    @Test
+    public void testMain() {
+        assertThatCode(() -> {
+        	MicrosservicoEmailApplication.main(new String[]{});
+        }).doesNotThrowAnyException();
+    }
+
 
 }
